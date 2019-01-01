e010f6db7a0d222e29a2cc6d84966cd17aa8d229
weifo <1641929622@qq.com>
2019-01-01 15:17:23 +0800

    Updates

---
+++ precache-manifest.c42bbcf30f586424b44393b9979e76ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/react-movie-search/static/media/tmdb.27b65cb4.svg"
+  },
+  {
+    "revision": "ee515d2504d4794968ae",
+    "url": "/react-movie-search/static/js/runtime~main.ee515d25.js"
+  },
+  {
+    "revision": "7ff8e28c9a0c31d3bb13",
+    "url": "/react-movie-search/static/js/main.7ff8e28c.chunk.js"
+  },
+  {
+    "revision": "e5b18f3d08bff35f4e33",
+    "url": "/react-movie-search/static/js/1.e5b18f3d.chunk.js"
+  },
+  {
+    "revision": "7ff8e28c9a0c31d3bb13",
+    "url": "/react-movie-search/static/css/main.f79cf3ff.chunk.css"
+  },
+  {
+    "revision": "8e0f21e445f32cd58e6205da89e61dd9",
+    "url": "/react-movie-search/index.html"
+  }
+];
